731eda0c4b6ab04a5b955748cb0e9359efc644f5
Juan Veronez <juan.monteirov@gmail.com>
2026-01-30 09:57:28 -0300

    feat: add logger using pino

--- server/src/env.ts
+++ server/src/env.ts
@@ -6,16 +6,6 @@ const envSchema = z.object({
   CLOUDFLARE_BUCKET: z.string(),
   CLOUDFLARE_ACCOUNT_ID: z.string(),
   CLOUDFLARE_PUBLIC_URL: z.string().url(),
-
-  OTEL_EXPORTER_OTLP_PROTOCOL: z.string(),
-  OTEL_EXPORTER_OTLP_ENDPOINT: z.string(),
-  OTEL_EXPORTER_OTLP_HEADERS: z.string(),
-  OTEL_TRACES_EXPORTER: z.string(),
-  OTEL_METRICS_EXPORTER: z.string(),
-  OTEL_LOGS_EXPORTER: z.string(),
-  NODE_OPTIONS: z.string(),
-  OTEL_RESOURCE_ATTRIBUTES: z.string(),
-  OTEL_NODE_RESOURCE_DETECTORS: z.string(),
 });
 
 export const env = envSchema.parse(process.env);
