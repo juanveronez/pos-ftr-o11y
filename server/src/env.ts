0d90ae9ea027d2d0fa93b959579f8feb9e40a223
Juan Veronez <juan.monteirov@gmail.com>
2026-01-30 07:47:07 -0300

    feat: add o11y envs validation and update server to include health check route

--- server/src/env.ts
+++ server/src/env.ts
@@ -1,4 +1,4 @@
-import z from "zod"
+import z from "zod";
 
 const envSchema = z.object({
   CLOUDFLARE_ACCESS_KEY_ID: z.string(),
@@ -6,6 +6,16 @@ const envSchema = z.object({
   CLOUDFLARE_BUCKET: z.string(),
   CLOUDFLARE_ACCOUNT_ID: z.string(),
   CLOUDFLARE_PUBLIC_URL: z.string().url(),
-})
 
-export const env = envSchema.parse(process.env)
+  OTEL_EXPORTER_OTLP_PROTOCOL: z.string(),
+  OTEL_EXPORTER_OTLP_ENDPOINT: z.string(),
+  OTEL_EXPORTER_OTLP_HEADERS: z.string(),
+  OTEL_TRACES_EXPORTER: z.string(),
+  OTEL_METRICS_EXPORTER: z.string(),
+  OTEL_LOGS_EXPORTER: z.string(),
+  NODE_OPTIONS: z.string(),
+  OTEL_RESOURCE_ATTRIBUTES: z.string(),
+  OTEL_NODE_RESOURCE_DETECTORS: z.string(),
+});
+
+export const env = envSchema.parse(process.env);
